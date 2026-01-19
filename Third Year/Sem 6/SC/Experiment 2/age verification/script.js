477710637eb11f835368f146c51d7d4d65f99e48
OmJoshi02 <omj020705@gmail.com>
2026-01-19 14:44:58 +0530

    Age verification

---
+++ Third Year/Sem 6/SC/Experiment 2/age verification/script.js
@@ -0,0 +1,10 @@
+let age = prompt("Enter your age : ")
+
+let age_num = Number(age)
+
+if(age_num > 18){
+    alert("You're eligible for vote")
+}
+else{
+    alert("You're not eligible for vote")
+}
